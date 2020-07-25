7b41e221391ce7bedc3ca17671a3a1722081de6b
Fahim Ahmed Shakil <fahimahmed38@gmail.com>
2020-07-26 03:54:13 +0600

    bound port variable to environment varibale

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.get('/contact', (req, res) => {
 
 
 //setting up listening port
-app.listen(8000, () => {
-    console.log('Listeing on port 8000');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log('Listeing on port ${PORT}...');
 });
